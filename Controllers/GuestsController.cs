b51ed05e51edcbf32cc19d0d2b5a5073558f66e1
Veselin Valkanov <veselin.d.valkanov@gmail.com>
2022-08-21 17:23:26 +0300

    added the api CRUD controller implementation

---
+++ Controllers/GuestsController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Project.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuestsController : ControllerBase
+    {
+        private List<GuestModel> guests = new List<GuestModel>()
+        {
+            new GuestModel() { Id = 1, FirstName="Veselin",LastName = "Valkanov" },
+            new GuestModel() { Id = 1, FirstName="Lychezara",LastName = "Karnolska" },
+            new GuestModel() { Id = 1, FirstName="Petar",LastName = "Petrov" },
+        };
+        // GET: api/<GuestsController>
+        [HttpGet]
+        public IEnumerable<GuestModel> Get()
+        {
+            return guests;
+        }
+
+        // GET api/<GuestsController>/5
+        [HttpGet("{id}")]
+        public ActionResult<GuestModel> Get(int id)
+        {
+            var res = guests.FirstOrDefault(x => x.Id == id);
+            if(res == null)
+            {
+                return BadRequest(res);
+            }
+            return Ok(res);
+        }
+
+        // POST api/<GuestsController>
+        [HttpPost]
+        public void Post([FromBody] GuestModel value)
+        {
+            var res = guests.FirstOrDefault(x => x.Id == value.Id);
+            if (res != null)
+            {
+                guests.Remove(res);
+            }
+            else
+            {
+                throw new Exception("the id is not valid");
+            }
+            guests.Add(value);
+        }
+
+        // PUT api/<GuestsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] GuestModel value)
+        {
+            guests.Add(value);  
+        }
+
+        // DELETE api/<GuestsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var res = guests.FirstOrDefault(x => x.Id == id);
+            if (res != null)
+            {
+                guests.Remove(res);
+            }
+            else
+            {
+                throw new Exception("the id is not valid");
+            }
+        }
+    }
+}
